76690decd6922abafe9049b0ff3f0fd13311276f
elessandro <elessandrodev@gmail.com>
2023-06-05 12:43:47 -0300

    create Model e Migration

---
+++ database/migrations/2023_06_05_133752_create_companies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('hero_id')->constrained('heroes');
+            $table->uuid('uuid');
+            $table->string('name', 100)->unique();
+            $table->string('email', 100)->unique();
+            $table->string('url')->unique();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
